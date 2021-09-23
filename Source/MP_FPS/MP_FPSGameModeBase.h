4204c6227c650eafd14f2e1c29313a58cbb99c50
Ali Aboshady <ali_aboshady@hotmail.com>
2021-09-23 23:42:07 +0100

    Initial Commit

---
+++ Source/MP_FPS/MP_FPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MP_FPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MP_FPS_API AMP_FPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
