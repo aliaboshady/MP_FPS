5572960a53ef81a306118978d78b8ef10972a515
Ali Aboshady <ali_aboshady@hotmail.com>
2021-10-03 02:51:32 +0100

    Added movement and mouse turn

--- Source/MP_FPS/CharacterMaster.cpp
+++ Source/MP_FPS/CharacterMaster.cpp
@@ -30,5 +30,41 @@ void ACharacterMaster::Tick(float DeltaTime)
 void ACharacterMaster::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACharacterMaster::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACharacterMaster::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &ACharacterMaster::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &ACharacterMaster::LookUp);
 }
 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////--- MOVEMENT ---////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+void ACharacterMaster::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void ACharacterMaster::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void ACharacterMaster::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ACharacterMaster::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+
+//UE_LOG(LogTemp, Warning, TEXT("Turn"));
